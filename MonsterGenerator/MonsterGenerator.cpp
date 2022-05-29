ae8338477886ff56d6af04e7599b8ac338926a49
oguzhankartal <kartaloguzhan12@gmail.com>
2022-05-29 15:07:56 +0200

    print function added

--- MonsterGenerator/MonsterGenerator.cpp
+++ MonsterGenerator/MonsterGenerator.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+#include <string_view>
 
 
 class Monster
@@ -28,8 +28,31 @@ private:
 
 public:
 
-    Monster(Type type, std::string name, std::string roar, int hitPoints) : m_type{ type }, m_name{ name }, m_roar{ roar }, m_hitPoints{ hitPoints }
+    Monster(Type type, std::string name, std::string roar, int hitPoints) 
+        : m_type{ type }, m_name{ name }, m_roar{ roar }, m_hitPoints{ hitPoints }
     {
+
+    }
+
+    std::string_view getTypeString() const
+    {
+        switch (m_type)
+        {
+        case Type::dragon: return "dragon";
+        case Type::goblin: return "goblin";
+        case Type::ogre: return "ogre";
+        case Type::orc: return "orc";
+        case Type::skeleton: return "skeleton";
+        case Type::troll: return "troll";
+        case Type::vampire: return "vampire";
+        case Type::zombie: return "zombie";
+        default: return "???";
+        }
+    }
+
+    void print() const
+    {
+        std::cout << m_name << " the " << getTypeString() << " has " << m_hitPoints << " hit points and says " << m_roar << '\n';
     }
 
 };
@@ -37,6 +60,8 @@ public:
 int main()
 {
     Monster skeleton{ Monster::Type::skeleton, "Bones", "rattle", 4 };
+
+    skeleton.print();
 
     return 0;
 }
