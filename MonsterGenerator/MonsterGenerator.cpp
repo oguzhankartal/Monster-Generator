b320d5e84180e1d89af540c0627e575fbf6d3824
oguzhankartal <kartaloguzhan12@gmail.com>
2022-05-29 15:02:18 +0200

    monstertype moved to monster class

--- MonsterGenerator/MonsterGenerator.cpp
+++ MonsterGenerator/MonsterGenerator.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 #include <string>
 
-enum MonsterType
-{
-    dragon,
-    goblin,
-    ogre,
-    orc,
-    skeleton,
-    troll,
-    vampire,
-    zombie,
 
-    max_monster_types
-};
 
 class Monster
 {
+public:
+    enum class Type
+    {
+        dragon,
+        goblin,
+        ogre,
+        orc,
+        skeleton,
+        troll,
+        vampire,
+        zombie,
+
+        max_monster_types
+    };
+
 private:
-    MonsterType m_type{};
+    Type m_type{};
     std::string m_name{};
     std::string m_roar{};
     int m_hitPoints{};
